6da679a610dd4b2ae6b089115cb4fd2df10c5e25
Vicr666 <970470332@qq.com>
2025-07-20 11:38:39 +0800

    Init

---
+++ docs/cooperation/sidebar_contents.js
@@ -0,0 +1,13 @@
+module.exports = [
+	{
+		title: "开源项目",
+		collapsable: true,
+		children:[
+			"/cooperation/project/PeiQi0-PeiQi-WIKI-Book",
+			"/cooperation/project/vulfocus",
+			"/cooperation/project/Goby",
+			"/cooperation/project/PortBrute",
+			"/cooperation/project/afrog",
+		]
+	},
+];
